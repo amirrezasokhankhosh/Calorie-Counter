6ca3da61d2e02f8d3738107b685427baedb6312a
Amirreza Sokhankhosh <amirrezasokhankhosh79@gmail.com>
2020-08-09 14:38:31 +0430

    with editing food

--- calorie_counter_app/urls.py
+++ calorie_counter_app/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     #SHOW ALL FOODS THAT USER ENTERED
-    url(r'^foods$' , views.foods , name='foods'),
+    url(r'^foods/$' , views.foods , name='foods'),
     url(r'^foods/(?P<food_id>\d+)/$', views.food, name='food'),
+    url(r'^new_food/$', views.new_food, name='new_food'),
+    url(r'^edit_food/(?P<food_id>\d+)/$', views.edit_food,name='edit_food'),
+
 ]
--- calorie_counter_app/views.py
+++ calorie_counter_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Food
+from .forms import FoodForm
 
 # Create your views here.
 def index(request):
@@ -28,3 +31,30 @@ def food(request , food_id):
     context = {'food' : food , 'name' : name , 'calories' : calories , 'carbohydrates' : carbohydrates , 'fat' : fat , 'fiber' : fiber , 'protein' : protein , 'saturated_fat' : saturated_fat}
     return render(request , 'calorie_counter_app/food.html' , context)
 
+def new_food(request):
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = FoodForm()
+    else:
+        # POST data submitted; process data.
+        form = FoodForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('calorie_counter_app:foods'))
+    context = {'form': form}
+    return render(request, 'calorie_counter_app/new_food.html', context)
+
+def edit_food(request, food_id):
+    """Edit an existing food."""
+    food = Food.objects.get(id=food_id)
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current food.
+        form = FoodForm(instance=food)
+    else:
+        # POST data submitted; process data.
+        form = FoodForm(instance=food, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('calorie_counter_app:food', args=[food.id]))
+    context = {'food' : food, 'form': form}
+    return render(request, 'calorie_counter_app/edit_food.html', context)
